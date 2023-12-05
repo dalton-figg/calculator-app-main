48851babe375f94627907facc03ff4ff964167cb
Dalton <dalton@stormcreative.co.uk>
2023-12-05 11:57:25 +0000

    Simplified calculator logic

--- app.js
+++ app.js
@@ -2,20 +2,6 @@ const calculator = document.getElementById("calculator");
 const screen = document.getElementById("calculator-screen");
 const keys = document.querySelectorAll(".calculator__key");
 
-// using an object as alternative to array so that the actual HTML of the buttons doesn't have to correspond to the JavaScript operators
-
-const operators = {
-  add: "+",
-  sub: "-",
-  multiply: "*",
-  div: "/",
-};
-
-const special = {
-  π: "3.14159265359",
-  e: "2.71828182846",
-};
-
 // can be changed for higher or lower accuracy
 
 const decimalAccuray = 3;
@@ -48,21 +34,6 @@ function executeAction(key, action) {
   // blanks out the screens innerHTML
   if (action == "reset") {
     screen.innerHTML = "";
-    calculator.dataset.prev = "";
-    calculator.dataset.operator = "";
-  }
-
-  // if the action is an operator, save the 'previous number' and clear the screen
-
-  if (action in operators) {
-    calculator.dataset.prev = screen.innerHTML;
-
-    if (screen.innerHTML in special) {
-      calculator.dataset.prev = special[screen.innerHTML];
-    }
-
-    calculator.dataset.operator = operators[action];
-    screen.innerHTML = "";
   }
 
   // grabs all the needed values and passes them out to be evaluated
@@ -76,39 +47,13 @@ function executeAction(key, action) {
       currentOperand = currentOperand.replace("π", "*3.14159265359");
     }
 
-    if (calculator.dataset.prev.includes("π")) {
-      calculator.dataset.prev = calculator.dataset.prev.replace(
-        "π",
-        "*3.14159265359"
-      );
-    }
-
     if (currentOperand.includes("e")) {
       currentOperand = currentOperand.replace("e", "*2.71828182846");
     }
 
-    if (calculator.dataset.prev.includes("e")) {
-      calculator.dataset.prev = calculator.dataset.prev.replace(
-        "e",
-        "*2.71828182846"
-      );
-    }
+    console.log(currentOperand);
 
-    // ensure nothing breaks when there isn't a number before any special chars
-
-    if (currentOperand == "*2.71828182846") {
-      currentOperand = "2.71828182846";
-    }
-
-    if (currentOperand == "*3.14159265359") {
-      currentOperand = "3.14159265359";
-    }
-
-    calculate(
-      calculator.dataset.prev,
-      calculator.dataset.operator,
-      currentOperand
-    );
+    calculate(currentOperand);
 
     calculator.dataset.prev = "";
     calculator.dataset.operator = "";
@@ -117,7 +62,5 @@ function executeAction(key, action) {
 
 // evaluates the expression, rounds it to 3 decimal places and removes any trailing zeros from the result
 
-calculate = (prevOperand, operator, currentOperand) =>
-  (screen.innerHTML = parseFloat(
-    eval(prevOperand + operator + currentOperand).toFixed(decimalAccuray)
-  ));
+calculate = (currentOperand) =>
+  (screen.innerHTML = parseFloat(eval(currentOperand).toFixed(decimalAccuray)));
